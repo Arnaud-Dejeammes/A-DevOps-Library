10a0232dd30e48f40bb43e919e95cd0f91c9bea3
Arnaud-Dejeammes <arnauddejeammes@gmail.com>
2023-09-13 18:22:26 +0200

    Settings for the automatic key pair generation and security

---
+++ Terraform_AWS/Create-EC2-Key-Pair/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+      # version = "4.15.0"
+    }
+  }  
+  # required_version = "1.5.6"
+}
+
+# 23/09/12 versions
--- Terraform_AWS/Create-EC2-Key-Pair/variables.tf
+++ Terraform_AWS/Create-EC2-Key-Pair/variables.tf
@@ -1,10 +1,16 @@
+# Safety tip:
+# Use the attribute and value `sensitive = true` when needed
+# (passwords, API keys, secret tokens...).
+# However, remember that the `.tfstate file` contains even 
+# the attribute values marked as sensitive.
+
 variable "region" {  
   default = "eu-north-1"
 }
 
-# Replace `default` value with the real user name
+# Replace `default` value with the real user name.
 variable "user" {
   description = "Output for the `user` resource"
   type    = string
-  default = "random_pet.pet_name.id"
+  default = "random_pet.pet_name.id"  
 }
